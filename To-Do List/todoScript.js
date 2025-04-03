319c5f0135b5871767d6ef83d121f84ed5d1a45c
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-04-03 22:53:38 +0530

    updated

---
+++ To-Do List/todoScript.js
@@ -0,0 +1,5 @@
+const todoInput = document.getElementById("todoInput");
+const addButton = document.getElementById("addTodoButton");
+const todoList = document.getElementById("todoList");
+
+addTodoBtn.addEventListener('click', addTodo);
